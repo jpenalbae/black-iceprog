4946579834ca52101bf2dcd3007a9f9b9ce43ca8
Juan Gonzalez-Gomez <juan@iearobotics.com>
2017-12-22 05:03:31 +0100

    Script refactored to be compatible with python3 (#1)
    Thanks I will upload this to pypi

--- blackiceprog/__main__.py
+++ blackiceprog/__main__.py
@@ -1,32 +1,42 @@
 import sys
 import serial
 
+
 def send_data(devname, filename):
-    wrote = 0
 
     try:
-        port = serial.Serial(devname)
-        bstream = open(filename, 'r')
+        # -- Open the serial port and configure its baudrate
+        with serial.Serial(devname) as port:
+            port.baudrate = 115200
 
-        while True:
-            data = bstream.read(1024)
-            if data == '':
-                break
-            wrote += port.write(data)
+            # -- Open the bitstream as a binary file, read the bytes and
+            # -- write them to the serial port
+            with open(filename, 'rb') as bstream:
+                data = bstream.read()
+                port.write(data)
 
-    except Exception as e:
-        print 'Error uploading bitstream'
-        print e
-        sys.exit(1)
+        # -- Report the sent bytes
+        print ("Wrote {} bytes".format(len(data)))
 
-    port.close()
-    bstream.close()
-    print 'Wrote %d bytes' % wrote
+    # -- Error managment. Something went wrong
+    except IOError as e:
+        errno, strerror = e.args
+        print("I/O error({0}): {1}".format(errno, strerror))
+
+    except serial.SerialException as e:
+        print("Serial error({}): ".format(e.args))
+
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
 
 
 def main():
     if len(sys.argv) != 3:
-        print 'Usage: %s serial_device bitstream_file' % sys.argv[0]
+        print ("Usage: {} serial_device bitstream_file".format(sys.argv[0]))
         sys.exit(1)
 
     send_data(sys.argv[1], sys.argv[2])
+
+
+if __name__ == "__main__":
+    main()
